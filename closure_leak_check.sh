e1b16522fed2fd50572f2a1af1530cea228289c8
Tomas Kohout <email@tomaskohout.cz>
2015-12-31 01:45:18 +0100

    Added support for multiple filees

--- closure_leak_check.sh
+++ closure_leak_check.sh
@@ -1,4 +1,10 @@
-#!/bin/bash  
+#!/usr/bin/env bash
+NEWLINE_SUBSTITUTE='`'
+FUNC_REGEX='/func.*?\{(.*?)(func.*)/ms'
+SELF_CLOSURE_REGEX='/(\{[^\}]*?\sin.*?\})(.*)/ms'
+HAS_SELF_REGEX='self\.'
+WEAK_REGEX="\[[[:space:]]*weak[[:space:]]*self[[:space:]]*\]"
+UNOWNED_REGEX="\[[[:space:]]*unowned[[:space:]]*self[[:space:]]*\]"
 
 function match_all {
     MATCHES=() 
@@ -24,81 +30,106 @@ function match_all {
 
 }
 
+function check_for_retain_cycles {
+	 filename=$1
 
-filename=""
-NEWLINE_SUBSTITUTE='`'
+
+    extension="${filename##*.}"
+	if [[ $extension == "swift" ]]; then
+
+	
+		file=`cat "$filename"`
+		
+
+		contents=$(cat "$filename" | tr '\n' "$NEWLINE_SUBSTITUTE")
+
+		match_all "$contents" "$FUNC_REGEX"
+		functions=( "${MATCHES[@]}" )
+
+		for match in "${functions[@]}"
+		do
+
+		   	match_all "$match" "$SELF_CLOSURE_REGEX"
+				closures=( "${MATCHES[@]}" )
+
+				for closure in "${closures[@]}"
+				do
+					if [[ $closure =~ $HAS_SELF_REGEX ]]; then
+							if ! [[ $closure =~ $WEAK_REGEX || $closure =~ $UNOWNED_REGEX ]] ; then
+				
+							
+							with_lines=`echo "$closure" | tr "$NEWLINE_SUBSTITUTE" '\n'`
+
+							first=`echo "$with_lines"|head -n 1`
+							possible_start=`grep -n "$first" $filename | cut -f1 -d:`
+							
+
+							
+							number_of_lines=`echo "$with_lines" | wc -l`
+							
+							while read -r start; do
+								found=true
+								for ((i = 1 ; i < $number_of_lines-1 ; i++ ));
+									do
+									
+									file_top=$(($i+$start-1))
+									file_line=`echo "$file" | head -n "$(($file_top+1))" | tail -1 | sed -e 's/^[ \t]*//'`
+									line=`echo "$with_lines" | head -n "$(($i+1))" | tail -1 | sed -e 's/^[ \t]*//'`
+
+									
+									if  [[ "$line" != "$file_line" ]] ; then
+										found=false
+										break
+									fi
+									done
+
+									if [[ $found == true ]]; then
+										filename_only="${filename##*/}"
+										echo -e "\e[31m\e[4mPossible strong retain cycle in \e[1m$filename_only\e[0m\e[31m\e[4m on line $start:\e[0m"
+										break
+									fi
+
+		    			done <<< "$possible_start"
+
+							#yellow_start='\\e[33m'
+							#yellow_end='\\e[0m'
+							
+							echo -e "$file" | head -n $(($start+$number_of_lines-1)) | tail -n $number_of_lines #|  sed "s/self/$yellow_startself$yellow_end/g"
+				   		#echo -e "\n\n"
+				   		fi
+					fi
+
+				done
+		done
+
+	fi
+}
+
+
+
+
+directory="."
 
 if [[ $1 ]]; then
-	filename=$1;
+	directory=$1;
 else
 	exit 1;
 fi
 
-contents=$(cat "$filename" | tr '\n' "$NEWLINE_SUBSTITUTE")
 
-
-FUNC_REGEX='/func.*?\{(.*?)(func.*)/ms'
-SELF_CLOSURE_REGEX='/(\{[^\}]*?\sin.*?\})(.*)/ms'
-HAS_SELF_REGEX='self\.'
-
-
-WEAK_REGEX="\[[[:space:]]*weak[[:space:]]*self[[:space:]]*\]"
-UNOWNED_REGEX="\[[[:space:]]*unowned[[:space:]]*self[[:space:]]*\]"
-
-
-match_all "$contents" "$FUNC_REGEX"
-functions=( "${MATCHES[@]}" )
-
-for match in "${functions[@]}"
-do
-
-   	
-	 	match_all "$match" "$SELF_CLOSURE_REGEX"
-		closures=( "${MATCHES[@]}" )
-
-		for closure in "${closures[@]}"
+if [ -d "$directory" ]; then
+	for name in $directory/*
 		do
-			if [[ $closure =~ $HAS_SELF_REGEX ]]; then
-					if ! [[ $closure =~ $WEAK_REGEX || $closure =~ $UNOWNED_REGEX ]] ; then
-		
-					
-					with_lines=`echo "$closure" | tr "$NEWLINE_SUBSTITUTE" '\n'`
-
-					first=`echo "$with_lines"|head -n 1`
-					possible_start=`grep -n "$first" $filename | cut -f1 -d:`
-					
-
-					file=`cat "$filename"`
-					number_of_lines=`echo "$with_lines" | wc -l`
-					
-					while read -r start; do
-						found=true
-						for ((i = 1 ; i < $number_of_lines-1 ; i++ ));
-							do
-							
-							file_top=$(($i+$start-1))
-							file_line=`echo "$file" | head -n "$(($file_top+1))" | tail -1 | sed -e 's/^[ \t]*//'`
-							line=`echo "$with_lines" | head -n "$(($i+1))" | tail -1 | sed -e 's/^[ \t]*//'`
-
-							
-							if  [[ "$line" != "$file_line" ]] ; then
-								found=false
-								break
-							fi
-							done
-
-							if [[ $found == true ]]; then
-								echo "Possible strong retain cycle on line $start:"
-								break
-							fi
-
-    			done <<< "$possible_start"
-
-					
-					echo "$file" | head -n $(($start+$number_of_lines-1)) | tail -n $number_of_lines
-		   		#echo -e "\n\n"
-		   		fi
-			fi
-
+			check_for_retain_cycles "$name"
 		done
-done
+else
+		check_for_retain_cycles "$directory"
+fi	
+
+
+
+
+
+
+
+
