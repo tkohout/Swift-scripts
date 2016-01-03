d2383a72adf36b0605500c2a079b0a5797803288
Tomas Kohout <email@tomaskohout.cz>
2016-01-03 20:54:12 +0100

    Refactoring

--- closure_leak_check.sh
+++ closure_leak_check.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 NEWLINE_SUBSTITUTE='`'
+nl=$NEWLINE_SUBSTITUTE
 FUNC_REGEX='/func.*?\{(.*?)(func.*)/ms'
-SELF_CLOSURE_REGEX='/('$NEWLINE_SUBSTITUTE'.*?\{[^\}]*?\sin.*?\})(.*)/ms'
+SELF_CLOSURE_REGEX='/\`([^\`]*?\{[^\}]*?\sin[\s\`].*?\})(.*)/ms'
 HAS_SELF_REGEX='self\.'
 WEAK_REGEX="\[[[:space:]]*weak[[:space:]]*self[[:space:]]*\]"
 UNOWNED_REGEX="\[[[:space:]]*unowned[[:space:]]*self[[:space:]]*\]"
+FOR_REGEX="[[:space:]]+for[[[:space:]]*"
+RETURN_REGEX="[[:space:]]+return[[[:space:]]*"
 
 function match_all {
     MATCHES=() 
@@ -30,6 +33,42 @@ function match_all {
 
 }
 
+function find_occurence_line_position {
+	local LINES=$1
+	local FIRST_LINE=`echo "$LINES"|head -n 1`	
+
+	local possible_start=`grep -n "$FIRST_LINE" $filename | cut -f1 -d:`
+	local LINES_NUMBER=`echo "$LINES" | wc -l`
+	
+
+
+	while read -r start; do
+
+		found=true
+		for ((i = 1 ; i < $LINES_NUMBER-1 ; i++ ));
+			do
+			
+			file_top=$(($i+$start-1))
+			file_line=`echo "$file" | head -n "$(($file_top+1))" | tail -1 | sed -e 's/^[ \t]*//'`
+			line=`echo "$LINES" | head -n "$(($i+1))" | tail -1 | sed -e 's/^[ \t]*//'`
+			
+			
+			if  [[ "$line" != "$file_line" ]] ; then
+				found=false
+				break
+			fi
+			done
+
+			if [[ $found == true ]]; then
+				
+				break
+			fi
+
+	done <<< "$possible_start"
+
+	LINE_POSITION=$start
+}
+
 function check_for_retain_cycles {
 	 filename=$1
 
@@ -48,54 +87,37 @@ function check_for_retain_cycles {
 
 		for match in "${functions[@]}"
 		do
+				
 
 		   	match_all "$match" "$SELF_CLOSURE_REGEX"
 				closures=( "${MATCHES[@]}" )
 
 				for closure in "${closures[@]}"
 				do
+
+
 					if [[ $closure =~ $HAS_SELF_REGEX ]]; then
-							if ! [[ $closure =~ $WEAK_REGEX || $closure =~ $UNOWNED_REGEX ]] ; then
-				
+
+							closure_newlines=`echo "$closure" | tr "$NEWLINE_SUBSTITUTE" '\n'`
+							first=`echo "$closure_newlines"|head -n 1`
+							number_of_lines=`echo "$closure_newlines" | wc -l`
+
+							if ! [[ $closure =~ $WEAK_REGEX || $closure =~ $UNOWNED_REGEX || $first =~ $FOR_REGEX || $first =~ $RETURN_REGEX ]] ; then
 							
-							with_lines=`echo "$closure" | tr "$NEWLINE_SUBSTITUTE" '\n'`
+							find_occurence_line_position "$closure_newlines"
 
-							first=`echo "$with_lines"|head -n 1`
-							possible_start=`grep -n "$first" $filename | cut -f1 -d:`
+		    			filename_only="${filename##*/}"
+		    			echo -e "\e[31m\e[4mPossible strong retain cycle in \e[1m$filename_only\e[0m\e[31m\e[4m on line $LINE_POSITION:\e[0m"
+
+		    			if [[ -n $LINE_POSITION ]]; then
+
 							
+							echo -e "$file" | head -n $(($start+$number_of_lines-1)) | tail -n $number_of_lines 
 
-							
-							number_of_lines=`echo "$with_lines" | wc -l`
-							
-							while read -r start; do
-								found=true
-								for ((i = 1 ; i < $number_of_lines-1 ; i++ ));
-									do
-									
-									file_top=$(($i+$start-1))
-									file_line=`echo "$file" | head -n "$(($file_top+1))" | tail -1 | sed -e 's/^[ \t]*//'`
-									line=`echo "$with_lines" | head -n "$(($i+1))" | tail -1 | sed -e 's/^[ \t]*//'`
-
-									
-									if  [[ "$line" != "$file_line" ]] ; then
-										found=false
-										break
-									fi
-									done
-
-									if [[ $found == true ]]; then
-										filename_only="${filename##*/}"
-										echo -e "\e[31m\e[4mPossible strong retain cycle in \e[1m$filename_only\e[0m\e[31m\e[4m on line $start:\e[0m"
-										break
-									fi
-
-		    			done <<< "$possible_start"
-
-							#yellow_start='\\e[33m'
-							#yellow_end='\\e[0m'
-							
-							echo -e "$file" | head -n $(($start+$number_of_lines-1)) | tail -n $number_of_lines #|  sed "s/self/$yellow_startself$yellow_end/g"
-				   		#echo -e "\n\n"
+							#Temporary bug fix
+				   	else
+				   			echo "$closure_newlines"
+				   	fi
 				   		fi
 					fi
 
@@ -106,8 +128,7 @@ function check_for_retain_cycles {
 }
 
 
-
-
+#Program
 directory="."
 
 if [[ $1 ]]; then
